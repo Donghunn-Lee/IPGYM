1c6a9e6bd9bd25a46aaa86c12c0737b11b6233fd
DDeunggeni <114406073+seunggeunchoi@users.noreply.github.com>
2023-05-21 21:32:10 +0900

    first!

--- src/App.js
+++ src/App.js
@@ -1,40 +1,33 @@
-import React from 'react';
-import MainPage from './components/MainPage';
-import './App.css';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-import MyPage from './components/MyPage';
-import PTticketInquiry from './components/PTticketInquiry';
-import PTReservation from './components/PTReservation';
-import MembershipManagement from './components/MembershipManagement';
-
-
-
-
+import React from "react";
+import MainPage from "./components/MainPage";
+import "./App.css";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import MyPage from "./components/MyPage";
+import PTticketInquiry from "./components/PTticketInquiry";
+import PTReservation from "./components/PTReservation";
+import MembershipManagement from "./components/MembershipManagement";
+import SignupForm from "./components/SignupForm";
+import Login from "./components/Login";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <div className="App">
+      <div className="App">
+        <header className="App-header"></header>
 
-        <header className='App-header'>
-    
-        </header>
-
-        <div className='App.body'>
-          
-            <Routes>
-              <Route path="/mypage" element={<MyPage/>}/>
-              <Route path="/ptticket" element={<PTticketInquiry/>}/>
-              <Route path="/ptreservation" element={<PTReservation/>}/>
-              <Route path="/membership" element={<MembershipManagement/>}/>
-              <Route path="/mainpage" element={<MainPage/>}/>
-              <Route path="/" element={<MainPage/>}/>
-            </Routes>
-
+        <div className="App.body">
+          <Routes>
+            <Route path="/signup" element={<SignupForm />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/mypage" element={<MyPage />} />
+            <Route path="/ptticket" element={<PTticketInquiry />} />
+            <Route path="/ptreservation" element={<PTReservation />} />
+            <Route path="/membership" element={<MembershipManagement />} />
+            <Route path="/mainpage" element={<MainPage />} />
+            <Route path="/" element={<MainPage />} />
+          </Routes>
         </div>
-
-
-    </div>
+      </div>
     </BrowserRouter>
   );
 };
