037198e73d25a455a8ba376a39418caa61470dd2
k1myeok <132213711+k1myeok@users.noreply.github.com>
2023-05-24 13:37:06 +0900

    adminlogin

--- src/components/MembershipManagement.js
+++ src/components/MembershipManagement.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './GymMembership.css';
+import './MembershipManagement.css';
 
 const MembershipManagement = () => {
   const [expirationDate, setExpirationDate] = useState('');
