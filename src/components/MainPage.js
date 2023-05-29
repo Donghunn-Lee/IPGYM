1de0474c56155b433fbfbcd1f86d707888173d9b
edenn-lee <93193485+edenn-lee@users.noreply.github.com>
2023-05-29 21:19:14 +0900

    폰트추가

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -8,40 +8,28 @@ const MainPage = () => {
     console.log(`Clicked ${feature}`);
   };
 
-  const [showSignupForm, setShowSignupForm] = useState(false); // 회원가입 폼을 나타내는 상태 변수
-
-  const toggleSignupForm = () => {
-    setShowSignupForm(!showSignupForm);
-  };
-
   return (
     <div className="main-page">
       <h1 className="main-title">Main Page</h1>
       <div className="button-container">
-        {/* <NavLink exact to="/login" activeClassName="active">
-          <Button text="로그인" onClick={() => handleButtonClick("로그인")} />
-        </NavLink> */}
         <NavLink exact to="/mypage" activeClassName="active">
           <Button
             text="마이페이지"
             onClick={() => handleButtonClick("마이페이지")}
           />
         </NavLink>
-        {/* <NavLink exact to="/ptticket" activeClassName="active">
-          <Button
-            text="PT 이용권"
-            onClick={() => handleButtonClick("PT 이용권")}
-          />
-        </NavLink> */}
+        
         <NavLink exact to="/pt" activeClassName="active">
           <Button text="PT 예약/조회" onClick={() => handleButtonClick("PT")} />
         </NavLink>
+
         <NavLink exact to="/membership" activeClassName="active">
           <Button
             text="헬스장 이용권"
             onClick={() => handleButtonClick("헬스장 이용권")}
           />
         </NavLink>
+
         <NavLink exact to="/bmi" activeClassName="active">
           <Button text="BMI 계산기" onClick={() => handleButtonClick("BMI")} />
         </NavLink>
