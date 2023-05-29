0ab84eb5974558432b4ecd3728e2405a9f12abfd
DDeunggeni <114406073+seunggeunchoi@users.noreply.github.com>
2023-05-29 21:34:56 +0900

    1

--- src/components/MembershipManagement.js
+++ src/components/MembershipManagement.js
@@ -7,6 +7,10 @@ const MembershipManagement = () => {
   const [usageHistory, setUsageHistory] = useState([]);
   const [selectedUsage, setSelectedUsage] = useState(null);
 
+  const handleButtonClick = (feature) => {
+    console.log(`Clicked ${feature}`);
+  };
+
   // 예제로 사용할 데이터
   const membershipData = {
     expirationDate: "2023-12-31",
@@ -14,6 +18,8 @@ const MembershipManagement = () => {
       { date: "2023-01-15", time: "10:30" },
       { date: "2023-02-05", time: "15:45" },
       { date: "2023-03-20", time: "18:20" },
+      { date: "2023-04-01", time: "13:00" },
+      { date: "2023-04-13", time: "13:20" },
     ],
   };
 
@@ -35,33 +41,39 @@ const MembershipManagement = () => {
   };
 
   return (
-    <div className="membership-container">
-      <h2 className="membership-heading">헬스장 이용권</h2>
-      <div className="membership-info">
-        <p className="info-label">회원권 만기 날짜:</p>
-        <p className="info-value">{expirationDate}</p>
+    <>
+      <div className="membership-container">
+        <h2 className="membership-heading">헬스장 이용권</h2>
+        <div className="membership-info">
+          <p className="info-label">회원권 만기 날짜:</p>
+          <p className="info-value">{expirationDate}</p>
+        </div>
+        <div className="membership-info">
+          <p className="info-label">만기까지 남은 날짜:</p>
+          <p className="info-value">{daysRemaining} 일</p>
+        </div>
       </div>
-      <div className="membership-info">
-        <p className="info-label">만기까지 남은 날짜:</p>
-        <p className="info-value">{daysRemaining} 일</p>
+
+      <div className="membership-container">
+        <h3 className="usage-heading">이용 기록</h3>
+        <p className="usage-allday">사용 일 수 : {usageHistory.length}일</p>
+        <ul className="usage-history">
+          {usageHistory.map((usage, index) => (
+            <li
+              key={index}
+              onClick={() => handleUsageClick(index)}
+              className={index === selectedUsage ? "selected" : ""}
+            >
+              {usage.date} (
+              {new Date(usage.date).toLocaleDateString("ko-KR", {
+                weekday: "long",
+              })}
+              ){index === selectedUsage && <span> - {usage.time}</span>}
+            </li>
+          ))}
+        </ul>
       </div>
-      <h3 className="usage-heading">이용 기록:</h3>
-      <ul className="usage-history">
-        {usageHistory.map((usage, index) => (
-          <li
-            key={index}
-            onClick={() => handleUsageClick(index)}
-            className={index === selectedUsage ? "selected" : ""}
-          >
-            {usage.date} (
-            {new Date(usage.date).toLocaleDateString("ko-KR", {
-              weekday: "long",
-            })}
-            ){index === selectedUsage && <span> - {usage.time}</span>}
-          </li>
-        ))}
-      </ul>
-    </div>
+    </>
   );
 };
 
