f1143098a418b4e0064b664071e6d71ab70562ce
DDeunggeni <114406073+seunggeunchoi@users.noreply.github.com>
2023-05-30 15:33:28 +0900

    회원정보검색

--- src/components/management/MemberManage.js
+++ src/components/management/MemberManage.js
@@ -1,30 +1,43 @@
 import React, { useState } from "react";
+import "./MemberManage.css";
 
 const MemberManage = () => {
-  // 회원가입 시 입력한 정보를 상태로 관리합니다.
-  const [name, setName] = useState("");
-  const [gender, setGender] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [searchName, setSearchName] = useState("");
+  const [memberInfo, setMemberInfo] = useState([
+    { name: "John Doe", gender: "Male", email: "johndoe@example.com" },
+    { name: "최승근", gender: "male", email: "janesmith@example.com" },
+    { name: "이동훈", gender: "male", email: "janesmith@example.com" },
+    { name: "김혁", gender: "male", email: "janesmith@example.com" },
+    { name: "전성권", gender: "male", email: "janesmith@example.com" },
+    { name: "정경완", gender: "male", email: "janesmith@example.com" },
+    { name: "박상혁", gender: "male", email: "janesmith@example.com" },
+    { name: "함석명", gender: "male", email: "함석명@example.com" },
+    { name: "함석명", gender: "Female", email: "함석명@example.com" },
 
-  // 이름 변경 핸들러
-  const handleNameChange = (e) => {
-    setName(e.target.value);
+    // ... 다른 회원 정보들
+  ]);
+
+  const handleSearch = () => {
+    // 검색 기능을 구현합니다.
+    const filteredMembers = memberInfo.filter((member) =>
+      member.name.includes(searchName)
+    );
+    setMemberInfo(filteredMembers);
   };
-
-  // 성별 변경 핸들러
-  const handleGenderChange = (e) => {
-    setGender(e.target.value);
-  };
-
-  // 이메일 변경 핸들러
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  // 비밀번호 변경 핸들러
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
+  const handleReset = () => {
+    // 검색 결과를 초기화합니다.
+    setSearchName("");
+    setMemberInfo([
+      { name: "John Doe", gender: "Male", email: "johndoe@example.com" },
+      { name: "최승근", gender: "male", email: "janesmith@example.com" },
+      { name: "이동훈", gender: "male", email: "janesmith@example.com" },
+      { name: "김혁", gender: "male", email: "janesmith@example.com" },
+      { name: "전성권", gender: "male", email: "janesmith@example.com" },
+      { name: "정경완", gender: "male", email: "janesmith@example.com" },
+      { name: "박상혁", gender: "male", email: "janesmith@example.com" },
+      { name: "함석명", gender: "male", email: "함석명@example.com" },
+      { name: "함석명", gender: "Female", email: "함석명@example.com" },
+    ]);
   };
 
   return (
@@ -36,37 +49,41 @@ const MemberManage = () => {
           <input
             className="info-input"
             type="text"
-            value={name}
-            onChange={handleNameChange}
+            value={searchName}
+            onChange={(e) => setSearchName(e.target.value)}
           />
         </label>
-        <label className="info-label">
-          성별:
-          <input
-            className="info-input"
-            type="text"
-            value={gender}
-            onChange={handleGenderChange}
-          />
-        </label>
-        <label className="info-label">
-          이메일:
-          <input
-            className="info-input"
-            type="text"
-            value={email}
-            onChange={handleEmailChange}
-          />
-        </label>
-        <label className="info-label">
-          비밀번호:
-          <input
-            className="info-input"
-            type="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </label>
+        <button onClick={handleSearch}>검색</button>
+        <button onClick={handleReset}>초기화</button>
+      </div>
+
+      <div className="table-container">
+        <table className="member-table">
+          <thead>
+            <tr>
+              <th>이름</th>
+              <th>성별</th>
+              <th>이메일</th>
+            </tr>
+          </thead>
+          <tbody>
+            {memberInfo.length > 0 ? (
+              memberInfo.map((member, index) => (
+                <tr key={index}>
+                  <td>{member.name}</td>
+                  <td>{member.gender}</td>
+                  <td>{member.email}</td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="3" className="no-matching-info">
+                  일치하는 회원 정보가 없습니다.
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
       </div>
     </div>
   );
